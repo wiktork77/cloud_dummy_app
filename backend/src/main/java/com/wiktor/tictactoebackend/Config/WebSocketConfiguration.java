5107231d23aba964df0ef1dc0fada17bdbd5bad4
Wiktor <wiktorkuberax@gmail.com>
2024-04-21 01:56:08 +0200

    xD

--- backend/src/main/java/com/wiktor/tictactoebackend/Config/WebSocketConfiguration.java
+++ backend/src/main/java/com/wiktor/tictactoebackend/Config/WebSocketConfiguration.java
@@ -1,5 +1,6 @@
 package com.wiktor.tictactoebackend.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
@@ -15,9 +16,11 @@ import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Value("${FRONT_ORIGIN:http://localhost:8081}")
+    private String frontOrigin;
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        String frontOrigin = "http://localhost:8081";
+        System.out.println(frontOrigin);
         registry.addEndpoint("/gameplay").setAllowedOrigins(frontOrigin).withSockJS();
     }
 
